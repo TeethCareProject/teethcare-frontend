9cf76d6a9edc3ce3924300208531ce40e93fbc66
Trong Nhan <nhantrongho2002@gmail.com>
2022-05-21 21:10:16 +0700

    Refactor code

--- src/components/ClinicForm/ClinicForm.jsx
+++ src/components/ClinicForm/ClinicForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Input, Button, Select } from "antd";
+import { Form, Input, Button, Select, Row, Col } from "antd";
 
 import {
   AttendantRegisterValidation,
@@ -22,72 +22,107 @@ const ClinicForm = () => {
       }}
       onFinish={onFinish}
     >
-      <div className="user-register-form">
-        <Form.Item name="username" rules={AttendantRegisterValidation.username}>
-          <Input placeholder="Username" />
-        </Form.Item>
-        <Form.Item name="password" rules={AttendantRegisterValidation.password}>
-          <Input type="password" placeholder="Password" />
-        </Form.Item>
-        <Form.Item
-          name="confirmPassword"
-          dependencies={["password"]}
-          rules={AttendantRegisterValidation.rePassword}
-        >
-          <Input type="password" placeholder="Confirm Password" />
-        </Form.Item>
-        <Form.Item
-          name="firstName"
-          rules={AttendantRegisterValidation.firstName}
-        >
-          <Input placeholder="First Name" />
-        </Form.Item>
+      <Row>
+        <Col>
+          <div className="user-register-form">
+            <Form.Item
+              name="username"
+              rules={AttendantRegisterValidation.username}
+            >
+              <Input placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={AttendantRegisterValidation.password}
+            >
+              <Input type="password" placeholder="Password" />
+            </Form.Item>
+            <Form.Item
+              name="confirmPassword"
+              dependencies={["password"]}
+              rules={AttendantRegisterValidation.rePassword}
+            >
+              <Input type="password" placeholder="Confirm Password" />
+            </Form.Item>
+            <Form.Item
+              name="firstName"
+              rules={AttendantRegisterValidation.firstName}
+            >
+              <Input placeholder="First Name" />
+            </Form.Item>
 
-        <Form.Item name="lastName" rules={AttendantRegisterValidation.lastName}>
-          <Input placeholder="Last Name" />
-        </Form.Item>
+            <Form.Item
+              name="lastName"
+              rules={AttendantRegisterValidation.lastName}
+            >
+              <Input placeholder="Last Name" />
+            </Form.Item>
 
-        <Form.Item>
-          <div className="gender-input">
-            <div className="gender-label">Gender</div>
-            <Select defaultValue="Male">
-              <Option value="Female">Female</Option>
-              <Option value="Others">Others</Option>
-            </Select>
+            <Form.Item>
+              <div className="gender-input">
+                <div className="gender-label">Gender</div>
+                <Select defaultValue="Male">
+                  <Option value="Female">Female</Option>
+                  <Option value="Others">Others</Option>
+                </Select>
+              </div>
+            </Form.Item>
+
+            <Form.Item name="phone" rules={AttendantRegisterValidation.phone}>
+              <Input placeholder="Phone number" />
+            </Form.Item>
           </div>
-        </Form.Item>
+        </Col>
+        <Col>
+          <div className="hospital-register-form">
+            <Form.Item
+              name="clinicName"
+              rules={ClinicRegisterValidation.clinicName}
+            >
+              <Input placeholder="Clinic's Name" />
+            </Form.Item>
+            <Form.Item
+              name="clinicTaxCode"
+              rules={ClinicRegisterValidation.taxId}
+            >
+              <Input placeholder="Clinic's Tax Code" />
+            </Form.Item>
+            <Form.Item
+              name="clinicPhoneNumber"
+              rules={ClinicRegisterValidation.phone}
+            >
+              <Input placeholder="Clinic's Phone Number" />
+            </Form.Item>
+            <Form.Item
+              name="clinicEmail"
+              rules={ClinicRegisterValidation.email}
+            >
+              <Input placeholder="Clinic's Email" />
+            </Form.Item>
 
-        <Form.Item name="phone" rules={AttendantRegisterValidation.phone}>
-          <Input placeholder="Phone number" />
-        </Form.Item>
-      </div>
-      <div className="hospital-register-form">
-        <Form.Item
-          name="clinicName"
-          rules={ClinicRegisterValidation.clinicName}
-        >
-          <Input placeholder="Clinic's Name" />
-        </Form.Item>
-        <Form.Item name="clinicTaxCode" rules={ClinicRegisterValidation.taxId}>
-          <Input placeholder="Clinic's Tax Code" />
-        </Form.Item>
-        <Form.Item
-          name="clinicPhoneNumber"
-          rules={ClinicRegisterValidation.phone}
-        >
-          <Input placeholder="Clinic's Phone Number" />
-        </Form.Item>
-        <Form.Item name="clinicEmail" rules={ClinicRegisterValidation.email}>
-          <Input placeholder="Clinic's Email" />
-        </Form.Item>
+            <Form.Item
+              name="clinicAddress"
+              rules={ClinicRegisterValidation.address}
+            >
+              <Input placeholder="Clinic's Address" />
+            </Form.Item>
 
-        <Form.Item
-          name="clinicAddress"
-          rules={ClinicRegisterValidation.address}
-        >
-          <Input placeholder="Clinic's Address" />
-        </Form.Item>
-
+            <Form.Item>
+              <div className="gender-input">
+                <div className="gender-label">Province</div>
+                <Select defaultValue="Province"></Select>
+              </div>
+            </Form.Item>
+            <Form.Item>
+              <div className="gender-input">
+                <div className="gender-label">District</div>
+                <Select defaultValue="District"></Select>
+              </div>
+            </Form.Item>
+          </div>
+        </Col>
+      </Row>
+      <Row justify="center">
         <Form.Item>
           <Button
             type="primary"
@@ -97,7 +132,7 @@ const ClinicForm = () => {
             Register
           </Button>
         </Form.Item>
-      </div>
+      </Row>
     </Form>
   );
 };
