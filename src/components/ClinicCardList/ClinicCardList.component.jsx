9abc6509d5f09cde8acb74d614370cff2d60a8bb
Trong Nhan <nhantrongho2002@gmail.com>
2022-06-07 17:20:19 +0700

    Refactor code

--- src/components/ClinicCardList/ClinicCardList.component.jsx
+++ src/components/ClinicCardList/ClinicCardList.component.jsx
@@ -1,27 +1,3 @@
-<<<<<<<< HEAD:src/containers/ClinicCard/ClinicCard.container.jsx
-import React, { Fragment, useState, useEffect } from "react";
-import { Col, Row, notification, Card } from "antd";
-import { StarFilled } from "@ant-design/icons";
-import RoutePath from "../../routers/Path";
-
-import clinicImg from "../../assets/clinicImg.png";
-import { useHistory, generatePath } from "react-router-dom";
-
-import { getClinics } from "../../services/teeth-apis/ClinicController";
-
-import ClinicCardComponent from "../../components/customized-components/ClinicCard/ClinicCard.component";
-
-const ClinicCardContainer = ({ clinicData, layoutDirection }) => {
-  const history = useHistory();
-
-  const handleClick = (clinicId) => {
-    history.push(
-      generatePath(RoutePath.CLINIC_DETAIL_PAGE, {
-        clinicId,
-      })
-    );
-  };
-========
 import React, { Fragment } from "react";
 import { Row, Col, Card } from "antd";
 
@@ -30,38 +6,9 @@ import { StarFilled } from "@ant-design/icons";
 
 import ClinicCardComponent from "../../components/customized-components/ClinicCard/ClinicCard.component";
 import "./ClinicCardList.style.scss";
->>>>>>>> TEET-86:src/components/ClinicCardList/ClinicCardList.component.jsx
 
 const ClinicCardListComponent = ({ layoutDirection, listData }) => {
   const { Meta } = Card;
-<<<<<<<< HEAD:src/containers/ClinicCard/ClinicCard.container.jsx
-  const [clinics, setClinics] = useState([]);
-
-  var filterClinicArray = clinicData || clinics;
-
-  const fetchingClinic = async () => {
-    try {
-      const { data } = await getClinics();
-      const mapperClinicData = data?.content?.map((clinic) => ({
-        ...clinic,
-        onClick: () => handleClick(clinic?.id),
-      }));
-      setClinics(mapperClinicData);
-    } catch (e) {
-      notification["error"]({
-        message: `Something went wrong! Try again latter!`,
-        description: `There is problem while fetching clinic, try again later`,
-        duration: 2,
-      });
-    }
-  };
-
-  useEffect(() => {
-    fetchingClinic();
-  }, []);
-
-========
->>>>>>>> TEET-86:src/components/ClinicCardList/ClinicCardList.component.jsx
   return (
     <Fragment>
       {layoutDirection === "row" ? (
@@ -107,14 +54,7 @@ const ClinicCardListComponent = ({ layoutDirection, listData }) => {
         <div className="card-clinic-preview">
           {listData?.map((clinic, index) => (
             <div key={index}>
-<<<<<<<< HEAD:src/containers/ClinicCard/ClinicCard.container.jsx
-              <ClinicCardComponent
-                clinic={clinic}
-                handleClick={clinic.onClick}
-              />
-========
               <ClinicCardComponent clinic={clinic} />
->>>>>>>> TEET-86:src/components/ClinicCardList/ClinicCardList.component.jsx
             </div>
           ))}
         </div>
