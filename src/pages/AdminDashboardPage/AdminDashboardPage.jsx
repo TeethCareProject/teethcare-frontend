4650be16ddb4c2393d3b3de676168645ffcacae0
Trong Nhan <nhantrongho2002@gmail.com>
2022-06-08 10:50:00 +0700

    Handle pending account logic

--- src/pages/AdminDashboardPage/AdminDashboardPage.jsx
+++ src/pages/AdminDashboardPage/AdminDashboardPage.jsx
@@ -2,23 +2,32 @@ import React, { useState } from "react";
 import DashboardLayout from "../../components/DashboardLayout/DashboardLayout.component";
 import { elementList, navigationList } from "./AdminDashboardPage.tabs";
 import { Space, Typography } from "antd";
+import { useSelector } from "react-redux";
+import AccountStatusConstants from "../../constants/AccountStatusConstants";
 
 const AdminDashboardPage = () => {
   const [currentTab, setCurrentTab] = useState(0);
+  const status = useSelector((state) => state.authentication.user?.status);
 
   return (
     <>
-      <Space style={{ padding: "1rem 0" }}>
-        <Typography.Title level={2}>Admin Dashboard</Typography.Title>
-      </Space>
-      <DashboardLayout
-        navigationList={navigationList.map((item, index) => ({
-          ...item,
-          onClick: () => setCurrentTab(index),
-        }))}
-        elementList={elementList}
-        currentTab={currentTab}
-      />
+      {status === AccountStatusConstants.PENDING ? (
+        <div>Your account is processing. Please waiting...</div>
+      ) : (
+        <div>
+          <Space style={{ padding: "1rem 0" }}>
+            <Typography.Title level={2}>Admin Dashboard</Typography.Title>
+          </Space>
+          <DashboardLayout
+            navigationList={navigationList.map((item, index) => ({
+              ...item,
+              onClick: () => setCurrentTab(index),
+            }))}
+            elementList={elementList}
+            currentTab={currentTab}
+          />
+        </div>
+      )}
     </>
   );
 };
