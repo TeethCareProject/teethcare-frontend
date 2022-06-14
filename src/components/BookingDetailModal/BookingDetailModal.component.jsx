e5500e3a7ce3d17017dbfc7af2499d0ed9c12358
kuroneko1011 <82007391+kuroneko1011@users.noreply.github.com>
2022-06-14 21:47:53 +0700

    remove check in in update booking modal

--- src/components/BookingDetailModal/BookingDetailModal.component.jsx
+++ src/components/BookingDetailModal/BookingDetailModal.component.jsx
@@ -53,7 +53,6 @@ const BookingDetailModalComponent = ({
       isUpdated ? (
         <UpdateBookingDetailModalContentContainer
           bookingData={bookingData}
-          checkInHandler={checkInHandler}
         />
       ) : (
         <BookingDetailModalContentComponent
